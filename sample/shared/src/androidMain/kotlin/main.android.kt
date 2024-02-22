58f026265e21909b6960d1a2ead41310b6271670
邹铭杰 <zoumingjie17@163.com>
2024-02-22 11:21:16 +0800

    feat:Android register AndroidLifecycleEventObserver

--- sample/shared/src/androidMain/kotlin/main.android.kt
+++ sample/shared/src/androidMain/kotlin/main.android.kt
@@ -1,5 +1,19 @@
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import com.multiplatform.lifecycle.LifecycleTracker
+import com.multiplatform.lifecyle.AndroidLifecycleEventObserver
 
 actual fun getPlatformName(): String = "Android"
 
-@Composable fun MainView() = App()
+@Composable fun MainView() {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(lifecycleOwner) {
+        val observer = AndroidLifecycleEventObserver(LifecycleTracker)
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+    App()
+}
