152c1cfcbd0d1d6fd903269a8936aaebfc728d06
邹铭杰 <zoumingjie17@163.com>
2024-02-22 11:19:12 +0800

    feat:LifecycleEvent

---
+++ lifecycletracker/src/commonMain/kotlin/com.multiplatform.lifecycle/LifecycleEvent.kt
@@ -0,0 +1,13 @@
+package com.multiplatform.lifecycle
+
+/**
+ * Created By Kevin Zou On 2024/2/22
+ */
+sealed class LifecycleEvent {
+    data object OnCreateEvent : LifecycleEvent()
+    data object OnStartEvent : LifecycleEvent()
+    data object OnResumeEvent : LifecycleEvent()
+    data object OnPauseEvent : LifecycleEvent()
+    data object OnStopEvent : LifecycleEvent()
+    data object OnDestroyEvent : LifecycleEvent()
+}
