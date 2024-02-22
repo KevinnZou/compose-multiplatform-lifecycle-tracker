594fab8174d07626481aad552cb3fc1389d2d8bd
邹铭杰 <zoumingjie17@163.com>
2024-02-22 11:19:47 +0800

    feat:LifecycleComposeUIVCDelegate

---
+++ lifecycletracker/src/iosMain/kotlin/com.multiplatform.lifecyle/LifecycleComposeUIVCDelegate.kt
@@ -0,0 +1,39 @@
+package com.multiplatform.lifecyle
+
+import androidx.compose.ui.uikit.ComposeUIViewControllerDelegate
+import com.multiplatform.lifecycle.LifecycleEvent
+import com.multiplatform.lifecycle.LifecycleTracker
+
+/**
+ * Created By Kevin Zou On 2024/2/22
+ */
+class LifecycleComposeUIVCDelegate(private val lifecycleTracker: LifecycleTracker) : ComposeUIViewControllerDelegate {
+    override fun viewDidAppear(animated: Boolean) {
+        super.viewDidAppear(animated)
+        println("OnResumeEvent")
+        lifecycleTracker.notifyListeners(LifecycleEvent.OnResumeEvent)
+    }
+
+    override fun viewDidDisappear(animated: Boolean) {
+        super.viewDidDisappear(animated)
+        lifecycleTracker.notifyListeners(LifecycleEvent.OnStopEvent)
+        lifecycleTracker.notifyListeners(LifecycleEvent.OnDestroyEvent)
+    }
+
+    override fun viewDidLoad() {
+        super.viewDidLoad()
+        println("OnCreateEvent")
+        lifecycleTracker.notifyListeners(LifecycleEvent.OnCreateEvent)
+    }
+
+    override fun viewWillAppear(animated: Boolean) {
+        super.viewWillAppear(animated)
+        println("OnStartEvent")
+        lifecycleTracker.notifyListeners(LifecycleEvent.OnStartEvent)
+    }
+
+    override fun viewWillDisappear(animated: Boolean) {
+        super.viewWillDisappear(animated)
+        lifecycleTracker.notifyListeners(LifecycleEvent.OnPauseEvent)
+    }
+}
