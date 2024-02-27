b3c749d4d87e1350c7eb205aad037397e44107f0
邹铭杰 <zoumingjie17@163.com>
2024-02-27 11:13:19 +0800

    feat:LifecycleEvent targetState

--- lifecycletracker/src/commonMain/kotlin/com.multiplatform.lifecycle/LifecycleEvent.kt
+++ lifecycletracker/src/commonMain/kotlin/com.multiplatform.lifecycle/LifecycleEvent.kt
@@ -15,4 +15,23 @@ sealed class LifecycleEvent {
     data object OnStopEvent : LifecycleEvent()
 
     data object OnDestroyEvent : LifecycleEvent()
+
+    /**
+     * Returns the new [Lifecycle.State] of a [Lifecycle] that just reported
+     * this [Lifecycle.Event].
+     *
+     * Throws [IllegalArgumentException] if called on [.ON_ANY], as it is a special
+     * value used by [OnLifecycleEvent] and not a real lifecycle event.
+     *
+     * @return the state that will result from this event
+     */
+    val targetState: State
+        get() {
+            return when (this) {
+                OnCreateEvent, OnStopEvent -> State.CREATED
+                OnStartEvent, OnPauseEvent -> State.STARTED
+                OnResumeEvent -> State.RESUMED
+                OnDestroyEvent -> State.DESTROYED
+            }
+        }
 }
