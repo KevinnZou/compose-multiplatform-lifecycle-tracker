539fab39d2b03f85b6d006808d2de25b4152deac
邹铭杰 <zoumingjie17@163.com>
2024-02-27 11:13:46 +0800

    feat:LifecycleTracker reconstruct

--- lifecycletracker/src/commonMain/kotlin/com.multiplatform.lifecycle/LifecycleTracker.kt
+++ lifecycletracker/src/commonMain/kotlin/com.multiplatform.lifecycle/LifecycleTracker.kt
@@ -3,18 +3,63 @@ package com.multiplatform.lifecycle
 /**
  * Created By Kevin Zou On 2024/2/22
  */
-object LifecycleTracker {
+open class LifecycleTracker: Lifecycle() {
+    /**
+     * Current state
+     */
+    private var state: State = State.INITIALIZED
     private val lifecycleObservers = mutableListOf<LifecycleObserver>()
 
-    fun addListener(listener: LifecycleObserver) {
-        lifecycleObservers.add(listener)
-    }
-
-    fun removeListener(listener: LifecycleObserver) {
-        lifecycleObservers.remove(listener)
-    }
-
-    fun notifyListeners(event: LifecycleEvent) {
+    private fun notifyListeners(event: LifecycleEvent) {
         lifecycleObservers.forEach { it.onEvent(event) }
+    }
+
+    override fun addObserver(observer: LifecycleObserver) {
+        if (state >= State.CREATED) {
+            observer.onEvent(LifecycleEvent.OnCreateEvent)
+        }
+        if (state >= State.STARTED) {
+            observer.onEvent(LifecycleEvent.OnStartEvent)
+        }
+        if (state >= State.RESUMED) {
+            observer.onEvent(LifecycleEvent.OnResumeEvent)
+        }
+
+        lifecycleObservers.add(observer)
+    }
+
+    override fun removeObserver(observer: LifecycleObserver) {
+        lifecycleObservers.remove(observer)
+    }
+
+    override var currentState: State
+        get() = state
+        /**
+         * Moves the Lifecycle to the given state and dispatches necessary events to the observers.
+         *
+         * @param state new state
+         */
+        set(state) {
+            moveToState(state)
+        }
+
+    /**
+     * Sets the current state and notifies the observers.
+     *
+     * Note that if the `currentState` is the same state as the last call to this method,
+     * calling this method has no effect.
+     *
+     * @param event The event that was received
+     */
+    open fun handleLifecycleEvent(event: LifecycleEvent) {
+        moveToState(event.targetState)
+        notifyListeners(event)
+    }
+
+    private fun moveToState(next: State) {
+        if (state == next) {
+            return
+        }
+        state = next
     }
 }
